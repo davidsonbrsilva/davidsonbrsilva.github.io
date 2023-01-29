8c7c2704b07ee3dd25c98f72a0ca4c6f3aa1d50a
Davidson Bruno <davidson.bruno@ufvjm.edu.br>
2023-01-29 12:23:47 -0300

    [#3] Finished header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,8 @@
 import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { languages } from "../locale";
+import AboutMeButton from "./AboutMeButton.vue";
+import ThemeModeButton from "./ThemeModeButton.vue";
 
 const i18n = useI18n();
 
@@ -14,18 +16,91 @@ const changeLocale = (locale: string) => {
 </script>
 
 <template>
-  <header>
-    <button type="button">{{ $t("header.aboutMeButton") }}</button>
-    <button type="button">{{ $t("header.switchToDarkModeButton") }}</button>
-    <button type="button" @click="isMenuOpen = !isMenuOpen">
-      {{ $t("header.chooseALanguageButton") }}
-    </button>
-    <menu v-if="isMenuOpen">
-      <li v-for="locale in $i18n.availableLocales">
-        <button type="button" @click="changeLocale(locale)">
-          {{ languages[locale as keyof typeof languages] }}
-        </button>
-      </li>
-    </menu>
+  <header class="header">
+    <AboutMeButton />
+    <nav class="nav">
+      <ThemeModeButton />
+      <button
+        class="language"
+        :class="{ active: isMenuOpen }"
+        type="button"
+        @click="isMenuOpen = !isMenuOpen"
+      >
+        <i class="material-symbols-outlined">translate</i
+        >{{ $t("header.chooseALanguageButton") }}
+      </button>
+      <menu class="menu" v-if="isMenuOpen">
+        <li v-for="locale in $i18n.availableLocales">
+          <button type="button" @click="changeLocale(locale)">
+            {{ languages[locale as keyof typeof languages] }}
+          </button>
+        </li>
+      </menu>
+    </nav>
   </header>
 </template>
+
+<style scoped>
+.header {
+  padding: 0.5rem 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.language {
+  font-size: 0;
+  padding: 0.5rem;
+  border-radius: 50%;
+  outline: none;
+  border: none;
+  color: #c9c9c9;
+  background: transparent;
+  cursor: pointer;
+  margin-left: 1rem;
+}
+
+.language:hover {
+  color: white;
+}
+
+.language.active {
+  color: #ffaa00;
+}
+
+.menu {
+  background-color: #262626;
+  border-radius: 0.5rem;
+  position: absolute;
+  top: 36px;
+  right: 0;
+  padding: 0.5rem 0;
+  overflow: hidden;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+}
+
+.menu li {
+  list-style: none;
+}
+
+.menu button {
+  width: 100%;
+  text-align: left;
+  padding: 0.5rem 1rem;
+  background: transparent;
+  outline: none;
+  border: none;
+  color: white;
+  cursor: pointer;
+}
+
+.menu button:hover {
+  background-color: #1a1a1a;
+}
+</style>
---
+++ src/components/ThemeModeButton.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isInLightMode = ref(false);
+</script>
+
+<template>
+  <input v-model="isInLightMode" type="checkbox" id="theme-mode-toggle" />
+  <label for="theme-mode-toggle">
+    <i class="material-symbols-outlined dark-mode">dark_mode</i>
+    <i class="material-symbols-outlined light-mode">light_mode</i>
+    <span>
+      {{
+        isInLightMode
+          ? $t("header.switchToLightModeButton")
+          : $t("header.switchToDarkModeButton")
+      }}
+    </span>
+  </label>
+</template>
+
+<style scoped>
+input {
+  width: 0;
+  height: 0;
+  visibility: hidden;
+}
+
+label {
+  width: 4rem;
+  height: 1.75rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  background: #262626;
+  border: 1px solid #333333;
+  border-radius: 1rem;
+  cursor: pointer;
+  padding: 0.125rem;
+  position: relative;
+}
+
+label:hover {
+  border: 1px solid #404040;
+}
+
+input:checked + label {
+  background-color: #ededed;
+  border: 1px solid #dbdbdb;
+  justify-content: flex-end;
+}
+
+input:checked + label:hover {
+  border: 1px solid #ededed;
+}
+
+label:after {
+  content: "";
+  width: 1.4rem;
+  height: 1.4rem;
+  border-radius: 0.8rem;
+  background-color: #1a1a1a;
+}
+
+input:checked + label:after {
+  background-color: #c9c9c9;
+}
+
+label,
+label:after {
+  transition: 0.3s;
+}
+
+label:active:after {
+  width: 1.75rem;
+}
+
+label i {
+  position: absolute;
+  font-size: 1rem;
+}
+
+label i.dark-mode {
+  opacity: 1;
+  left: 4px;
+  color: #c9c9c9;
+}
+
+label i.light-mode {
+  opacity: 0;
+  right: 4px;
+}
+
+input:checked + label i.dark-mode {
+  opacity: 0;
+}
+
+input:checked + label i.light-mode {
+  color: #1a1a1a;
+  opacity: 1;
+}
+
+label span {
+  font-size: 0;
+}
+</style>
